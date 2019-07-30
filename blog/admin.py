f053042bbd845568997385bf3f32aaed79153921
Exopole <alexandre.nicaise28@gmail.com>
2019-07-30 15:43:47 +0800

    add admin in blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+def apply_publish(modeladmin, request, queryset):
+    for post in queryset:
+        post.publish()
+
+apply_publish.short_description = 'Publish'
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'published_date', 'author']
+    actions = [apply_publish, ]  # <-- Add the list action function here
+    #fields = ['author', 'title', 'created_date', 'published_date']
+
+
+admin.site.register(Post, PostAdmin)
+#admin.site.register(Post)

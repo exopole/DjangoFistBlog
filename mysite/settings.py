4ff35adb8c7eb8323183b180761fc0772d300e63
Exopole <alexandre.nicaise28@gmail.com>
2019-07-30 09:23:25 +0800

    debug false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z$7&auh$p$@#f5*tz&7y_hp29t3+*s%c8-xhdm(kf@k&8@7o78'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
